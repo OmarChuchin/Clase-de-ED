9a5b021e6d429f913c1e383fc758f38586a59aeb
Unknown <A01378844@itesm.mx>
2017-08-27 01:20:32 -0500

    Modificacion no.1

--- src/Practica_2/SLinkedList.java
+++ src/Practica_2/SLinkedList.java
@@ -24,7 +24,7 @@ public class SLinkedList<E> implements List<E> {
                 else{
                     this.top.next=n;
                     n.next=null;}
-                size++;
+                this.size++;
 	}
 
 	@Override
@@ -39,51 +39,91 @@ public class SLinkedList<E> implements List<E> {
                 current.next=n;
                 n.next=null;
                 }
-                size++;}
+                this.size++;}
 
 	@Override
 	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-		
+            if(index<0 || index>=this.size)
+                throw new IndexOutOfBoundsException();
+            if(index==0)
+                this.addFirst(element);
+            else{
+                int in=0;
+                SNode<E> n=new SNode(element),
+                        bef=this.top.next;
+                while(in<index){
+                    in++;
+                    bef=bef.next;
+                }
+                n.next=bef.next;
+                bef.next=n;
+            }
+            this.size++;
 	}
 
 	@Override
 	public E removeFirst() {
-                if(this.isEmpty()==false){
-                    SNode<E> el=this.top.next;
-                    this.top.next=el.next;
-                    el.next=null;
-                    size--;
-                }
-                else
+                if(this.isEmpty())
                     throw new NoSuchElementException();
-                return null;
+                this.size--;
+                SNode<E> current=this.top.next;
+                this.top.next=current.next;
+                current.next=null;
+                return current.value;
 	}
 
 	@Override
 	public E removeLast() {
-                if(this.isEmpty()==false){
-                    SNode<E> current=this.top.next;
-                    SNode<E> last=current.next;
-                    while(last.next!=null){
-                        current=current.next;
-                        last=last.next;
-                    }
-                    current.next=null;size--;
+                if(this.isEmpty())
+                    throw new NoSuchElementException();
+                this.size--;
+                SNode<E> current=this.top.next,
+                    last=current.next;
+                while(last.next!=null){
+                    current=current.next;
+                    last=last.next;
                 }
-		return null;
+                current.next=null;
+                return last.value;
+                
 	}
 
 	@Override
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            if(index>=this.size|| index<0)
+                throw new IndexOutOfBoundsException();
+            if(index==0){
+                this.removeFirst();
+            }
+            SNode<E> current=this.top.next;
+            int in=0;
+            while(in<index-1){
+                current=current.next;
+                in++;
+            }
+            SNode<E> last=current.next;
+            current.next=last.next;
+            last.next=null;
+            return last.value;
+            
+        }
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		SNode<E> current=this.top.next;
+                int index=0;
+                while(current.next!=null){
+                    if(current.value.equals(o)){
+                        this.remove(index);
+                        return true;
+                    }
+                    index++;
+                }
+                if(current.value.equals(o)){
+                    this.removeLast();
+                    return true;
+                }
+                return false;
 	}
 
 	@Override
@@ -105,8 +145,8 @@ public class SLinkedList<E> implements List<E> {
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+            
+            return null;
 	}
 
 	@Override
